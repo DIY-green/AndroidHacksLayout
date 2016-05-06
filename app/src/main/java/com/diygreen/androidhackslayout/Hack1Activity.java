eb381cdb7689743d900b8b783a8a46203cb1836b
liwangcheng <liwangcheng@jiashuangkuaizi.com>
2016-05-06 17:00:01 +0800

    添加布局切换

--- app/src/main/java/com/diygreen/androidhackslayout/Hack1Activity.java
+++ app/src/main/java/com/diygreen/androidhackslayout/Hack1Activity.java
@@ -1,7 +1,7 @@
 package com.diygreen.androidhackslayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class Hack1Activity extends AppCompatActivity {
